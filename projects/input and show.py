57c9d3f6886bbde891f99806f9a340a3c3700fca
Shishir <bseeseer@gmail.com>
2026-01-18 19:37:43 +0545

    added for basic read/write

---
+++ projects/input and show.py
@@ -0,0 +1,18 @@
+import cv2
+# Read the image
+a=input("enter the name of image")
+img = cv2.imread(a)
+
+# Check if image is loaded
+if img is None:
+    print("Image not found!")
+else:
+    # Show the image
+    cv2.imshow("My Image", img)
+
+    # Wait until a key is pressed
+    # cv2.waitKey(0)
+    cv2.waitKey(5000)  # waits 5000 milliseconds (5 seconds)
+
+    # Close all OpenCV windows
+    cv2.destroyAllWindows()
